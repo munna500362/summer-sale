83caf860c01a8ce76ecf834db6fd027961c1a4c0
mehedi hasan <mehedihasan500362@gmail.com>
2024-08-22 13:29:39 +0900

    final commit

--- js/summer.js
+++ js/summer.js
@@ -1,33 +1,78 @@
 let total = 0;
 function myfunction(target){
-    const productName = document.getElementById("div1");
-   const itemName = target.parentNode.childNodes[1].childNodes[7].innerText;
-   const li = document.createElement('li');
-    li.innerText = itemName;
-    productName.appendChild(li);
+    
+    const productPrizeStr =
+      target.childNodes[3].childNodes[3].childNodes[3].innerText.split(" ")[0];
+    //   console.log(productPrizeStr)
+    const productPrizeNum = parseFloat(productPrizeStr);
+    const totalPrize = document.getElementById("total-prize");
+    total += productPrizeNum;
+    totalPrize.innerText = total;
+    totalPrize.innerHTML = `${total.toFixed(2)}`;
 
-    // const price = target.parentNode.childNodes[1].childNodes[9].innerText.split(' ')[0];
-    const price = target.parentNode.childNodes[1].childNodes[9].innerText.split(' ')[0];
+    const productName = target.childNodes[3].childNodes[3].childNodes[1].innerText;
 
-    // console.log(price)
-    total = parseFloat(total)+parseFloat(price);
-    // console.log(total)
-    //  totalPrice = parseFloat(price) + parseFloat(total);
-   const TotalPrice =document.getElementById("totalvalue").innerText=total; 
-//    if(TotalPrice>=200){
+    const productList = document.getElementById("product-list");
+  
+    const li = document.createElement("p");
+    const counter = productList.childElementCount;
+    li.innerHTML = `<b>${counter + 1}. ${productName}</b>`;
+  
+    productList.appendChild(li);
+  
+     // *********************
+  const makePurchaseBtn = document.getElementById("make-purchase");
+  const totalPrizeValue = parseFloat(totalPrize.innerText);
 
-//    }
- 
+  if (totalPrizeValue > 1) {
+    makePurchaseBtn.removeAttribute("disabled");
+  }
+  const applyBtn = document.getElementById("apply-btn");
+  const discount = document.getElementById("discount");
+  const discountPrize = totalPrizeValue * (20 / 100);
+  const totalTK = document.getElementById("total");
+  const totalTkPay = totalPrizeValue - discountPrize;
+  const InputFiled = document.getElementById("coupon-code").value;
+
+  if (totalPrizeValue > 200) {
+    applyBtn.removeAttribute("disabled");
+
+    if (totalPrizeValue > 200) {
+      applyBtn.removeAttribute("disabled");
+
+      
+    }
    
-}
-function myButton(){
-    const basicBtn = document.getElementById("basic-btn");
-    if(TotalPrice>=200){
-        basicBtn.disabled = false;
-    }
-    else{
-        basicBtn.disabled = true;
-    }
-    console.log("hellow")
+  }
+
+  return totalPrizeValue;
+ 
 }
 
+const applyBin=document.getElementById("apply-btn");
+applyBin.addEventListener('click',function(){
+  const discount = document.getElementById("discount");
+  const totalTK = document.getElementById("total");
+  const InputFiled = document.getElementById("coupon-code").value;
+  const InputFiled1 = document.getElementById("coupon-code").value='';
+  
+        const discountPrize = total * (20 / 100);
+
+        if (InputFiled === "SELL200") {
+          discount.innerText = discountPrize.toFixed(2);
+          totalTK.innerText = (total - discountPrize).toFixed(2);
+        } else {
+          alert("Please input the correct value");
+        }
+})
+
+function goHome() {
+  window.location.href = "index.html";
+}
+
+// function he(){
+//   return 5;
+
+// }
+
+// console.log(cardBox())
